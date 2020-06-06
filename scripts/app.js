0e6bb8bfd5a6c9cc6f12cfbbfc6e2e16ec15937a
Abros <abros.nahat@bk.ru>
2020-06-06 22:57:17 +0300

    add adaptive first slide

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+particlesJS.load('particles-js', 'particles.json');
+
+particlesJS.load('particles-js2', 'particles2.json');
+
+particlesJS.load('particles-js3', 'particles.json');
+
+particlesJS.load('particles-js4', 'particles2.json');
---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    sectionsColor: ['#252525', '#fff', '#121314', '#fff'],
+    scrollingSpeed: 900,
+    scrollBar: false,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#myMenu',
+    loopBottom: true
+});
+
+const down = document.querySelector('.down');
+
+down.addEventListener('click', () => fullpage_api.moveSectionDown())
